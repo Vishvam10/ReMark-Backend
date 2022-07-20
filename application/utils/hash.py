db78e5c5dd0518767614949c3037e48b58846cd3
Vishvam <s.vishvam1025@gmail.com>
2022-07-20 21:47:26 +0530

    Changed the way API Keys are generated

--- application/utils/hash.py
+++ application/utils/hash.py
@@ -2,6 +2,9 @@ import string
 import random
 import bcrypt
 import uuid
+import secrets
+
+from application.utils.validation import BusinessValidationError
 
 def create_hashed_password(password) :
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
@@ -10,9 +13,11 @@ def check_hashed_password(password, hashed_password) :
     return bcrypt.checkpw(password.encode('utf8'), hashed_password.encode('utf8'))
 
 def generate_api_key(length):
-    char_set = string.ascii_letters + string.punctuation                    
-    urand = random.SystemRandom()                                           
-    return ''.join([urand.choice(char_set) for _ in range(length)])
+    if(length < 16) :
+        raise BusinessValidationError(status_code=400, error_message="Insufficient length")
+    
+    api_key = secrets.token_urlsafe(length)                                 
+    return api_key
 
 def generate_random_id() :
     return str(uuid.uuid4()).replace("-", "")
