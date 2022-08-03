ce1989df8ca06737fd8bbdc1b680a39fc0547415
Vishvam <s.vishvam1025@gmail.com>
2022-08-03 23:57:26 +0530

    Added User Preference Model

---
+++ application/models/UserPreference.py
@@ -0,0 +1,34 @@
+import datetime
+
+from dataclasses import dataclass
+from email.policy import default
+from application.database import db
+
+from sqlalchemy.sql import func
+
+@dataclass
+class UserPrefence(db.Model):
+    __tablename__ = "user_preference"
+
+    user_id : str
+    show_resolved : bool
+    show_moderated_comments : bool
+    comments_limit_per_annotation : int
+    default_theme : str
+    brand_colors : str
+  
+   
+    user_id = db.Column(db.String, unique=True, nullable=False, primary_key=True)
+
+    show_resolved = db.Column(db.Boolean, unique=False, default=False)
+    show_moderated_comments = db.Column(db.Boolean, unique=False, default=False)
+    comments_limit_per_annotation = db.Column(db.Integer, unique=False, default=10)    
+
+    default_theme = db.Column(db.String, unique=False, default="light")
+
+    # This is a JSON string 
+    brand_colors = db.Column(db.String, unique=False, nullable=True)
+
+   
+    def to_dict(self):
+        return dict(user_id=self.user_id, show_resolved=self.show_resolved, show_moderated_comments=self.show_moderated_comments, comments_limit_per_annotation=self.comments_limit_per_annotation, default_theme=self.default_theme, brand_colors=self.brand_colors)
