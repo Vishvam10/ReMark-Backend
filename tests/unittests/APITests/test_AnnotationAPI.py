9ffaabb90755499982326fbb260953f4f7138b24
Vishvam <s.vishvam1025@gmail.com>
2022-08-21 01:58:43 +0530

    Wrote tests for Annotation API

--- tests/unittests/APITests/test_AnnotationAPI.py
+++ tests/unittests/APITests/test_AnnotationAPI.py
@@ -4,10 +4,12 @@ from application.database.database import db
 from application.models.User import User
 from application.models.Token import Token
 from application.models.Website import Website
+from application.models.Annotation import Annotation
 
 BASE_API_URL = "/api"
 USER_API_URL = "{}/user".format(BASE_API_URL)
 WEBSITE_API_URL = "{}/website".format(BASE_API_URL)
+ANNOTATION_API_URL = "{}/annotation".format(BASE_API_URL)
 
 ADMIN_DATA = {
     "username": "test_admin",
@@ -23,13 +25,29 @@ WEBSITE_DATA = {
     "admin_type" : "BASIC"
 }
 
+ANNOTATION_DATA = {
+    "website_id" : "",
+    "website_uri" : "127.0.0.1:5000/",
+    "annotation_name" : "Sample Title",
+    "node_xpath" : "//html/body/div[0]",
+    "html_id" : "sampleTest",
+    "html_tag" : "div",
+    "html_text_content" : "Sample",
+    "user_id" : "",
+    "user_name" : "test_admin",
+    "tags" : "test1,test2,test3"
+}
+
 HEADERS = None
 
 TESTING_ADMIN_ID = ""
 TESTING_WEBSITE_ID = ""
+TESTING_ANNOTATION_ID = ""
 
-def test_0_init_admin(client, auth) :
+def test_0_init(client, auth) :
     global TESTING_ADMIN_ID
+    global TESTING_WEBSITE_ID
+    global ANNOTATION_DATA
     global WEBSITE_DATA
     global HEADERS
     user = db.session.query(User).filter(User.username == ADMIN_DATA["username"]).first()
@@ -55,3 +73,86 @@ def test_0_init_admin(client, auth) :
         "Authorization" : "Bearer {}".format(TESTING_ACCESS_TOKEN),
         "API_KEY" : API_KEY
     }
+    TESTING_WEBSITE_ID = json.loads(client.post(WEBSITE_API_URL, json=WEBSITE_DATA, headers=HEADERS).data).get("data").get("website_id")
+    ANNOTATION_DATA["user_id"] = TESTING_ADMIN_ID
+    ANNOTATION_DATA["website_id"] = TESTING_WEBSITE_ID
+    
+def test_1_create_annotation(client) :
+    global ANNOTATION_DATA
+    global TESTING_ANNOTATION_ID
+    res = json.loads(client.post(ANNOTATION_API_URL, json=ANNOTATION_DATA, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("status") == 201
+    assert res.get("message") == "New Annotation Created"
+
+    TESTING_ANNOTATION_ID = res.get("data").get("annotation_id")
+
+def test_2_get_annotation_by_annotation_id(client) :
+    global ANNOTATION_DATA
+    url = "{}/{}".format(ANNOTATION_API_URL, TESTING_ANNOTATION_ID)
+    res = json.loads(client.get(url, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("annotation_id") == TESTING_ANNOTATION_ID
+
+def test_3a_edit_annotation_name(client) :
+    global ANNOTATION_DATA
+    url = "{}/{}".format(ANNOTATION_API_URL, TESTING_ANNOTATION_ID)
+    NEW_ANNOTATION_DATA = {
+		"action_type" : "edit_name",
+		"new_name" : "Edited Title",
+		"new_tags" : "",
+		"new_resolved" : ""
+    }
+    res = json.loads(client.put(url, json=NEW_ANNOTATION_DATA, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("status") == 201
+    assert res.get("message") == "Annotation edited successfully !"
+
+def test_3b_edit_annotation_tags(client) :
+    global ANNOTATION_DATA
+    url = "{}/{}".format(ANNOTATION_API_URL, TESTING_ANNOTATION_ID)
+    NEW_ANNOTATION_DATA = {
+		"action_type" : "edit_tags",
+		"new_name" : "",
+		"new_tags" : "edit1,edit2",
+		"new_resolved" : ""
+    }
+    res = json.loads(client.put(url, json=NEW_ANNOTATION_DATA, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("status") == 201
+    assert res.get("message") == "Annotation edited successfully !"
+
+def test_3c_edit_annotation_resolve(client) :
+    global ANNOTATION_DATA
+    url = "{}/{}".format(ANNOTATION_API_URL, TESTING_ANNOTATION_ID)
+    NEW_ANNOTATION_DATA = {
+		"action_type" : "edit_resolved",
+		"new_name" : "",
+		"new_tags" : "",
+		"new_resolved" : True
+    }
+    res = json.loads(client.put(url, json=NEW_ANNOTATION_DATA, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("status") == 201
+
+def test_3d_edit_annotation_resolve(client) :
+    global ANNOTATION_DATA
+    url = "{}/{}".format(ANNOTATION_API_URL, TESTING_ANNOTATION_ID)
+    NEW_ANNOTATION_DATA = {
+		"action_type" : "edit_resolved",
+		"new_name" : "",
+		"new_tags" : "",
+		"new_resolved" : True
+    }
+    res = json.loads(client.put(url, json=NEW_ANNOTATION_DATA, headers=HEADERS).data)
+    assert res is not None
+    assert res.get("status") == 201
+
+def test_4_cleanup() :
+    global TESTING_ADMIN_ID
+    db.session.query(Annotation).delete()
+    db.session.query(Website).filter(Website.website_id == TESTING_WEBSITE_ID).delete()
+    print("TEST 4 RESPONSE : ", TESTING_ADMIN_ID)
+    db.session.query(Token).filter(Token.user_id == TESTING_ADMIN_ID)
+    db.session.query(User).filter(User.user_id == TESTING_ADMIN_ID)
+    db.session.commit()
